ed3792986fe9435d35769aae069f229fa812900f
smss123 <smsssamer@live.com>
2014-05-14 02:38:56 -0700

    Creeat ing Pages

---
+++ Gulf-futureWeb/WebSite/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebSite.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="styles/kendo.common.min.css" rel="stylesheet" />
+    <link href="styles/kendo.metro.min.css" rel="stylesheet" />
+    <script src="Scripts/jquery-2.1.0.min.js"></script>
+    <script src="Scripts/js/kendo.web.min.js"></script>
+   
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+           
+            $("#Grid").kendoGrid({
+                columns: [{
+                    field: "name",// create a column bound to the "name" field
+                    title: "Name" // set its title to "Name"
+                }, {
+                    field: "age",// create a column bound to the "age" field
+                    title: "Age" // set its title to "Age"
+                }],
+                dataSource: [{ name: "Jane", age: 30 }, { name: "John", age: 33 }]
+            });
+
+            $("#x").hide(1000);
+
+        });
+    </script>    
+  
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="Grid">
+    
+    </div>
+        <div id="x">samer abd ulla</div>
+    </form>
+</body>
+</html>
--- Gulf-futureWeb/WebSite/WebSiteControlPanal/bestDealsControl.aspx.designer.cs
+++ Gulf-futureWeb/WebSite/WebSiteControlPanal/bestDealsControl.aspx.designer.cs
@@ -11,5 +11,104 @@ namespace WebSite.WebSiteControlPanal {
     
     
     public partial class bestDealsControl {
+        
+        /// <summary>
+        /// RadStyleSheetManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadStyleSheetManager RadStyleSheetManager1;
+        
+        /// <summary>
+        /// RadScriptManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadScriptManager RadScriptManager1;
+        
+        /// <summary>
+        /// RadAjaxManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadAjaxManager RadAjaxManager1;
+        
+        /// <summary>
+        /// RadGrid2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadGrid RadGrid2;
+        
+        /// <summary>
+        /// EntityDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource2;
+        
+        /// <summary>
+        /// RadGrid3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadGrid RadGrid3;
+        
+        /// <summary>
+        /// EntityDataSource3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource3;
+        
+        /// <summary>
+        /// RadGrid4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadGrid RadGrid4;
+        
+        /// <summary>
+        /// EntityDataSource4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource4;
+        
+        /// <summary>
+        /// RadGrid1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadGrid RadGrid1;
+        
+        /// <summary>
+        /// EntityDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.EntityDataSource EntityDataSource1;
     }
 }
